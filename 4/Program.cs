dc0f17f20b7a68fce8d6b6e9ccc02fed3a91082b
Kolesnik Anton <kolesnikas2006@gmail.com>
2022-10-17 22:02:33 +0500

    111

---
+++ 4/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+int a = 2;
+int b = 7;
+int c = 3;
+if (a > b & a > c)
+{
+    Console.WriteLine(a);
+}
+else if (b > a & b > c)
+{
+    Console.WriteLine(b);
+}
+else
+{
+    Console.WriteLine(c);
+}
